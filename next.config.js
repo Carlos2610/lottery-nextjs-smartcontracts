7c2bd7058ecdea0990c1ee568d59f9d30aa586d0
cako <cakorocamora@gmail.com>
2022-07-25 19:54:54 +0200

    exported

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        images: {
+            unoptimized: true, //esto lo hemos añadido para crear un sitio estatico
+        },
+    },
 }
 
 module.exports = nextConfig
